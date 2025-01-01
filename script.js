9cfe1ac8f3fa89267af4670b367c27aa46569810
Your Name <fauziyyanuraahmad@gmail.com>
2025-01-01 20:38:17 +0100

    update script.js

--- script.js
+++ script.js
@@ -12,22 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-/*document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById('menuToggle');
-    const closeMenu = document.getElementById('closeMenu');
-    const navMenu = document.getElementById('navMenu');
-
-    if (menuToggle && navMenu && closeMenu) {
-        menuToggle.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
-        });
-
-        closeMenu.addEventListener('click', () => {
-            navMenu.classList.remove('active');
-        });
-    }
-});*/
-
 const sections = [
     {
         title: "Why choose us",
